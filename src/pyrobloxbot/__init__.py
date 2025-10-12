672af12fa5148b8050abeb71bbddcc7ee26b86e0
skiddude <themrdudydude2021@gmail.com>
2025-10-12 22:30:21 +0300

    fix: use slash key '/' to open chat
    the old code used shift+7 (in most keyboards outputs "&") for opening the chat.
    
    the new code uses "/" instead.

--- src/pyrobloxbot/__init__.py
+++ src/pyrobloxbot/__init__.py
@@ -298,10 +298,7 @@ def chat(message:str):
     :type message: str
     """
     #Open chat
-    dinput.keyDown("shift")
-    dinput.keyDown("7")
-    dinput.keyUp("shift")
-    dinput.keyUp("7")
+    dinput.press("/")
 
     #Use clipboard to paste message quickly
     previousClipboard = pyclip.paste()
