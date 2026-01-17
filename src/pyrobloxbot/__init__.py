895cae013fb0003a94dd8eefe60236495640a50f
António Charrão <60406199+Mews@users.noreply.github.com>
2026-01-17 15:12:19 +0000

    Remove comment rules

--- src/pyrobloxbot/__init__.py
+++ src/pyrobloxbot/__init__.py
@@ -1,22 +1,22 @@
 """pyrobloxbot public API exports."""
 
-from .exceptions import *  # noqa: F401,F403
-from .literals import *  # noqa: F401,F403
-from .core import *  # noqa: F401,F403
+from .exceptions import *
+from .literals import *
+from .core import *
 
 try:
     from .exceptions import __all__ as _exceptions_all
-except ImportError:  # pragma: no cover
+except ImportError:
     _exceptions_all = []
 
 try:
     from .literals import __all__ as _literals_all
-except ImportError:  # pragma: no cover
+except ImportError:
     _literals_all = []
 
 try:
     from .core import __all__ as _core_all
-except ImportError:  # pragma: no cover
+except ImportError:
     _core_all = []
 
 __all__ = [*_exceptions_all, *_literals_all, *_core_all]
