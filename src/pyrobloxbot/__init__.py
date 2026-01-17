563831d63439ce68fbee6d38602c63edca4108c0
António Charrão <60406199+Mews@users.noreply.github.com>
2026-01-17 15:24:01 +0000

    Remove silencing `ImportError` in `__init__.py`
    We shouldn't be silencing `ImportError` when importing the module, these should be raised as usual

--- src/pyrobloxbot/__init__.py
+++ src/pyrobloxbot/__init__.py
@@ -4,19 +4,8 @@ from .exceptions import *
 from .literals import *
 from .core import *
 
-try:
-    from .exceptions import __all__ as _exceptions_all
-except ImportError:
-    _exceptions_all = []
-
-try:
-    from .literals import __all__ as _literals_all
-except ImportError:
-    _literals_all = []
-
-try:
-    from .core import __all__ as _core_all
-except ImportError:
-    _core_all = []
+from .exceptions import __all__ as _exceptions_all
+from .literals import __all__ as _literals_all
+from .core import __all__ as _core_all
 
 __all__ = [*_exceptions_all, *_literals_all, *_core_all]
