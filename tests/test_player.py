00898a6e2550abf11bdd1a2c7fa1d7738140c832
Mews <60406199+Mews@users.noreply.github.com>
2026-01-25 00:59:13 +0000

    Add unit tests for most bot methods (94% coverage)

---
+++ tests/test_player.py
@@ -0,0 +1,70 @@
+import pytest
+from unittest.mock import patch
+import pyrobloxbot as bot
+
+
+class TestPlayer:
+    @pytest.fixture(autouse=True)
+    def setup_mocks(self):
+        with patch("pyrobloxbot.core.dinput") as self.mock_dinput:
+            yield
+
+    def test_reset_player(self):
+        bot.reset_player(interval=1)
+
+        self.mock_dinput.press.assert_called_once_with(
+            ("esc", "r", "enter"), interval=1
+        )
+
+    def test_leave_game(self):
+        bot.leave_game(interval=1)
+
+        self.mock_dinput.press.assert_called_once_with(
+            ("esc", "l", "enter"), interval=1
+        )
+
+    def test_toggle_shift_lock(self):
+        bot.toggle_shift_lock()
+
+        self.mock_dinput.press.assert_called_once_with("shift")
+
+    def test_equip_slot(self):
+        bot.equip_slot(5)
+
+        self.mock_dinput.press.assert_called_once_with("5")
+
+    def test_equip_slot_invalid_slot(self):
+        with pytest.raises(bot.exceptions.InvalidSlotNumberException):
+            bot.equip_slot(11)
+
+    @patch("pyrobloxbot.core.pyclip")
+    def test_chat(self, mock_pyclip):
+        original_clipboard_content = "original clipboard"
+
+        mock_pyclip.paste.return_value = original_clipboard_content
+        test_message = "hello everyone"
+
+        bot.chat(test_message)
+
+        from unittest.mock import call
+
+        mock_pyclip.copy.assert_has_calls(
+            [call(test_message), call(original_clipboard_content)]
+        )
+
+        self.mock_dinput.press.assert_has_calls([call("/"), call("enter")])
+
+        self.mock_dinput.keyDown.assert_has_calls([call("ctrl"), call("v")])
+        self.mock_dinput.keyUp.assert_has_calls([call("ctrl"), call("v")])
+
+    @patch("pyrobloxbot.core.os.system")
+    def test_launch_game(self, mock_os_system):
+        game_id = 123456
+        bot.state.UI_NAV_ENABLED = True
+
+        bot.launch_game(game_id)
+
+        expected_command = f"start roblox://placeId={game_id}"
+        mock_os_system.assert_called_once_with(command=expected_command)
+
+        assert bot.state.UI_NAV_ENABLED is False
