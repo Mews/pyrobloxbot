93808fd67bc6e94b968fcf13c006455bd869fa0e
Mews <60406199+Mews@users.noreply.github.com>
2026-02-17 13:30:20 +0000

    (#111) Fix wait_for_focus docstring

--- src/pyrobloxbot/core/window.py
+++ src/pyrobloxbot/core/window.py
@@ -2,10 +2,13 @@ import win32gui
 
 
 def wait_for_focus() -> int:
-    """Waits until the Roblox window is in focus, and returns it's window handle.
+    """Waits until any Roblox window is in focus, and returns it's window handle.
+
+    Note:
+        Window handles are volatile. If you close an app and reopen it, it's handle will likely have changed.
 
     Returns:
-        int: The window handle for the Roblox window that made the function exit. A window handle is a unique integer and is used by Windows to identify windows.
+        int: The window handle for the Roblox window that made the function exit. A window handle is a unique integer that is used to identify a specific window.
     """
 
     hwnd = win32gui.GetForegroundWindow()
